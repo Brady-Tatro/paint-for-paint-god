a98afb6c604645663b35a659673b8f5d2b08b6cc
Brady Tatro <bradytatro@gmail.com>
2016-11-13 13:48:02 -0500

    added basic tests and facotry

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,5 +1,5 @@
 class Color < ActiveRecord::Base
 
   validates :name, presence: true
-
+  validates :hex, length: { is: 7 }
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,1 +1,8 @@
 require 'factory_girl_rails'
+
+FactoryGirl.define do
+  factory :color do
+    sequence(:name) { |n| "Blood#{n} for the blood god" }
+    sequence(:hex) { |n| "#834F#{n}4"}
+  end
+end
